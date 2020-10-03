37c6452ed0c6fadf77c9f2599c7264aac454e181
sebuyase@gmail.com <sebuyase@gmail.com>
2020-10-03 18:48:58 -0500

    Revert "Breaking down functions for reusability. Created Ingredients type."
    This reverts commit 084e7bce9e03ffdf016bc6cee737dc6e4fcf14e4.

--- Program.fs
+++ Program.fs
@@ -1,14 +1,57 @@
-﻿open Ingredients
+﻿// Learn more about F# at http://fsharp.org
 
-let ingredients =
-    {
-    Starter = 289.0; // grams
-    StarterHydration = 1.0; // 1.0 = 100%
-    Water = 260.2619048; // grams
-    Flour = 450.7380952; // grams
-    Salt = 9.0909; // grams
-    }
+open System
 
-let hydration = ingredients.Hydration
+[<EntryPoint>]
+let main argv =
+    printf "Calculate hydration (h) or flour/water/salt components (c)?";
+    let calcBranch = Console.ReadLine();
 
-let ingredients2 = Components(289.0, 1.0, 0.68, 1000.0, 0.909090)
+    if calcBranch = "h" then
+        printf "Starter (g):";
+        let starter = Console.ReadLine();
+        printf "Starter hydration (%%):";
+        let starterHydration = Console.ReadLine();
+        printf "Water (g):";
+        let water = Console.ReadLine();
+        printf "Flour (g):";
+        let flour = Console.ReadLine();
+
+        let s = (float)starter;
+        let sH = (float)starterHydration / 100.0;
+        let w = (float)water;
+        let f = (float)flour;
+
+        let sD = s / (sH + 1.0);
+        let sW = (sH * s) / (sH + 1.0);
+        let wet = sW + w;
+        let dry = sD + f;
+        let hydration = wet / dry;
+
+        printfn "Dough has %f%% hydration" (hydration * 100.0);
+    elif calcBranch = "c" then
+        printf "Starter (g):";
+        let starter = Console.ReadLine();
+        printf "Starter hydration (%%):";
+        let starterHydration = Console.ReadLine();
+        printf "Desired dough hydration (%%):"
+        let hydration = Console.ReadLine();
+        printf "Desired dough mass (g):";
+        let mass = Console.ReadLine();
+
+        let s = (float)starter;
+        let sH = (float)starterHydration / 100.0;
+        let h = (float)hydration / 100.0;
+        let m = (float)mass;
+
+        let sD = s / (sH + 1.0);
+        let sW = (sH * s) / (sH + 1.0);
+        let flour = (m - sD - (h * sD)) / (h + 1.0);
+        let water = m - sD - sW - flour;
+        let salt = (m * 0.909090) / 100.0;
+
+        printfn "flour (g):%f" flour;
+        printfn "water (g):%f" water;
+        printfn "salt (tsp):%f" (salt / 2.3);
+    
+    0 // return an integer exit code
