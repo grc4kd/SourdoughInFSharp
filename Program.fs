e391d92097ae9b3a3a5a3fc4f93405ce80022fe3
sebuyase@gmail.com <sebuyase@gmail.com>
2020-10-03 21:50:47 -0500

    Added tests before program entry point. Failure aborts the program.

--- Program.fs
+++ Program.fs
@@ -26,8 +26,50 @@ let Components starter starterHydration desiredHydration desiredMass =
         Salt = (desiredMass * 0.909090) / 100.0
     }
 
+// tests before entrypoint
+let testDecimal (expected : decimal) (actual : decimal) (threshold : decimal) =
+    let difference = abs (expected - actual)
+    match difference < threshold with
+    | true -> 
+        true
+    | false -> 
+        printfn "test failed, expected: %M, actual %M" expected actual
+        false
+
+let result1 = testDecimal 0.68M (decimal (Hydration 289.0 1.0 260.261905 450.738095)) 0.000001M
+match result1 with
+| true -> () // empty unit, do nothing if test passed
+| false -> printfn "func Hydration test failed"
+
+let testIngredientsExpected =
+    {
+        Starter = 289.0
+        Water = 260.261905
+        Flour = 450.738095
+        Salt = 9.0909
+    } 
+let testIngredientsActual = Components 289.0 1.0 0.68 1000.0
+
+let result2 = testDecimal (decimal testIngredientsExpected.Starter) (decimal testIngredientsActual.Starter) 0.000001M
+let result3 = testDecimal (decimal testIngredientsExpected.Water) (decimal testIngredientsActual.Water) 0.000001M
+let result4 = testDecimal (decimal testIngredientsExpected.Flour) (decimal testIngredientsActual.Flour) 0.000001M
+let result5 = testDecimal (decimal testIngredientsExpected.Salt) (decimal testIngredientsActual.Salt) 0.000001M
+
+let testsList = [ result1; result2; result3; result4; result5 ]
+
+match result2, result3, result4, result5 with
+| true, true, true, true -> () // empty unit, do nothing if test passed
+| _, _, _, _ -> printfn "func Components test failed"
+
 [<EntryPoint>]
 let main argv =
+    // break if some test did not pass
+    if not (List.contains false testsList) then
+        "ok"
+    else
+        failwith "Some program test(s) did not pass."
+    |> ignore // explicitly ignore string output of the above if/else function
+
     printf "Calculate hydration (h) or flour/water/salt components (c)?";
     let calcBranch = Console.ReadLine();
 
